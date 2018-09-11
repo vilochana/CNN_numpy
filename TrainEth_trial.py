72c4525bc46cac32f88de9e8ae071f88fe65dca7
vilochana <me.vilochana@gmail.com>
2018-09-11 18:47:55 +0530

    trying_pushing changes

---
+++ TrainEth_trial.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+CHARS= "0123"
+
+def char_to_vec(c):
+    y = np.zeros(len(CHARS))
+    y[c] =1
+
+import cv2 
+for fname in glob.glob(data_folder+)
+im = cv2.imread(fname)
+im = cv2.resize(im, (128,128))/255.
+cv2.imwrite("/home/ubuntu/Extra200GB/research_vilochana/Ethnicity/test_data/resized.jpg",img)
+
+
+
+import cv2
+import glob
+data_folder = "/home/ubuntu/Extra200GB/research_vilochana/Ethnicity/"
+for fname in random.sample(glob.glob(data_folder+"specs_data/*/*"):
+    im = cv2.imread(fname)
+    new_name = data_folder+'specs_data_renamed/specs_'+fname.split('/')[-1]
+    print new_name
+    cv2.imwrite(new_name,im)
+
